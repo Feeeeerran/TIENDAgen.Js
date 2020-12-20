04f26b1d35b4136fa123af5891a5f2857a0cec18
Feeeeerran <oheydudeman@gmail.com>
2020-12-20 04:13:51 -0300

    Fin de la lógica que relaciona el nav-bar con las productos. Merge a master por este logro eee

---
+++ assets/js/nav.js
@@ -0,0 +1,24 @@
+// Por si algún pillo quiere entrar a producto.html de primeras
+// defino un navDir por defecto 
+localStorage.navDir=0; 
+
+// motherboards
+$("ul.navbar-nav>li:nth-child(2)").click(function() {
+	localStorage.navDir=0;
+});
+// ram
+$("ul.navbar-nav>li:nth-child(3)").click(function() {
+	localStorage.navDir=1;
+});
+// grafica
+$("ul.navbar-nav>li:nth-child(4)").click(function() {
+	localStorage.navDir=2;
+});
+// procesador
+$("ul.navbar-nav>li:nth-child(5)").click(function() {
+	localStorage.navDir=3;
+});
+// perifericos
+$("ul.navbar-nav>li:nth-child(6)").click(function() {
+	localStorage.navDir=4;
+})
